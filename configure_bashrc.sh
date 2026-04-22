374e14f5610a3a64358df0aa29a576bbf752e3a5
William Barber <wobarber@outlook.com>
2026-04-22 16:48:20 -0700

    Fix typos from configure_bashrc.sh

--- configure_bashrc.sh
+++ configure_bashrc.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-if [[ ! -d "~/.container_setup.sh" ]]; then
+if [[ ! -f "$(echo $HOME)/.container_setup.sh" ]]; then
 	cp ./.container_setup.sh ~/.container_setup.sh
 	chmod +x ~/.container_setup.sh;
 fi
 
-if [[ -z $(grep "source $(echo $HOME)/.container_setup.sh" "$(echo $HOME)/.bashrc") ]]; then 
+if [[ -z $(grep "source ~/.container_setup.sh" "$(echo $HOME)/.bashrc") ]]; then 
 	echo "" >> ~/.bashrc # Newline
 	echo "# For distrobox container setup:" >> ~/.bashrc
 	echo "source ~/.container_setup.sh" >> ~/.bashrc;
