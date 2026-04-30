190da377c219f36844d96c99343874ecad3bf1e1
William Barber <wobarber@outlook.com>
2026-04-29 21:30:14 -0700

    Add warning message for poweroff

--- .container_setup.sh
+++ .container_setup.sh
@@ -6,20 +6,25 @@ fi
 
 
 if [ -n "$CONTAINER_ID" ] || [ -n "$DISTROBOX_ENTER_PATH" ]; then
-    PICO_SCRIPT_PATH=$(find ~ -type d -maxdepth 5 -name Pico-Scripts 2> /dev/null)
     if [ -x /usr/bin/dircolors ]; then
         alias ls='ls --color=auto'
         alias grep='grep --color=auto'
-	if [ -v PICO_SCRIPT_PATH ]; then
-		alias kill.sh="distrobox-host-exec $PICO_SCRIPT_PATH/kill.sh"
-		alias start.sh="distrobox-host-exec $PICO_SCRIPT_PATH/start.sh"
-		alias ping.sh="distrobox-host-exec $PICO_SCRIPT_PATH/ping.sh"
-		alias status.sh="distrobox-host-exec $PICO_SCRIPT_PATH/status.sh"
-		alias stop_thrusters.sh="distrobox-host-exec $PICO_SCRIPT_PATH/stop_thrusters.sh"
-		alias thruster_test.sh="distrobox-host-exec $PICO_SCRIPT_PATH/thruster_test.sh"
-		alias all_thrusters.sh="distrobox-host-exec $PICO_SCRIPT_PATH/all_thrusters.sh"
-	fi
     fi
+
+    PICO_SCRIPT_PATH=$(find ~ -type d -maxdepth 5 -name Pico-Scripts 2> /dev/null)
+    if [ -v PICO_SCRIPT_PATH ]; then
+    	alias kill.sh="distrobox-host-exec $PICO_SCRIPT_PATH/kill.sh"
+    	alias start.sh="distrobox-host-exec $PICO_SCRIPT_PATH/start.sh"
+    	alias ping.sh="distrobox-host-exec $PICO_SCRIPT_PATH/ping.sh"
+    	alias status.sh="distrobox-host-exec $PICO_SCRIPT_PATH/status.sh"
+    	alias stop_thrusters.sh="distrobox-host-exec $PICO_SCRIPT_PATH/stop_thrusters.sh"
+    	alias thruster_test.sh="distrobox-host-exec $PICO_SCRIPT_PATH/thruster_test.sh"
+    	alias all_thrusters.sh="distrobox-host-exec $PICO_SCRIPT_PATH/all_thrusters.sh"
+    fi
+
+    alias poweroff="echo \"'poweroff' must be run from host. Type EOF (ctrl+d) to exit the container.\""
+    alias sudo="sudo " # Allows bash to recognise sudo aliases
+
     if [ $USR_SHELL = "bash" ]; then
 	    PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
     fi
