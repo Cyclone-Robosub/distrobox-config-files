6a80df32803ed525b67e1ded3df7c0d0d4e5dc0a
William Barber <wobarber@outlook.com>
2026-05-02 13:58:35 -0700

    Remove software killswitch stuff

--- .container_setup.sh
+++ .container_setup.sh
@@ -13,10 +13,7 @@ if [ -n "$CONTAINER_ID" ] || [ -n "$DISTROBOX_ENTER_PATH" ]; then
 
     PICO_SCRIPT_PATH=$(find ~ -type d -maxdepth 5 -name Pico-Scripts 2> /dev/null)
     if [ -v PICO_SCRIPT_PATH ]; then
-    	alias kill.sh="distrobox-host-exec $PICO_SCRIPT_PATH/kill.sh"
-    	alias start.sh="distrobox-host-exec $PICO_SCRIPT_PATH/start.sh"
     	alias ping.sh="distrobox-host-exec $PICO_SCRIPT_PATH/ping.sh"
-    	alias status.sh="distrobox-host-exec $PICO_SCRIPT_PATH/status.sh"
     	alias stop_thrusters.sh="distrobox-host-exec $PICO_SCRIPT_PATH/stop_thrusters.sh"
     	alias thruster_test.sh="distrobox-host-exec $PICO_SCRIPT_PATH/thruster_test.sh"
     	alias all_thrusters.sh="distrobox-host-exec $PICO_SCRIPT_PATH/all_thrusters.sh"
