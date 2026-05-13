8322c9c866fee4ef3fb4039f922a1f15461caec2
William Barber <wobarber@outlook.com>
2026-05-12 18:38:15 -0700

    Update _ to - for consistency

--- configure-bashrc.sh
+++ configure-bashrc.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-if [[ ! -f "$(echo $HOME)/.container_setup.sh" ]]; then
-	cp ./.container_setup.sh ~/.container_setup.sh
-	chmod +x ~/.container_setup.sh;
+if [[ ! -f "$(echo $HOME)/.container-setup.sh" ]]; then
+	cp ./.container-setup.sh ~/.container-setup.sh
+	chmod +x ~/.container-setup.sh;
 fi
 
 USR_SHELL=$(echo $SHELL | awk -F  '/' '{print $NF}')
 
-if [[ -z $(grep "source ~/.container_setup.sh" "$(echo $HOME)/.$(echo $USR_SHELL)rc") ]]; then 
+if [[ -z $(grep "source ~/.container-setup.sh" "$(echo $HOME)/.$(echo $USR_SHELL)rc") ]]; then 
 	echo "" >> ~/.bashrc # Newline
 	echo "# For distrobox container setup:" >> ~/.bashrc
-	echo "source ~/.container_setup.sh" >> ~/.bashrc;
+	echo "source ~/.container-setup.sh" >> ~/.bashrc;
 fi
