85c9c814861e9681872163f2cf799711f100b60d
William Barber <wobarber@outlook.com>
2026-05-15 18:14:01 -0700

    old pre-docker version

---
+++ configure_bashrc.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+if [[ ! -f "$(echo $HOME)/.container_setup.sh" ]]; then
+	cp ./.container_setup.sh ~/.container_setup.sh
+	chmod +x ~/.container_setup.sh;
+fi
+
+USR_SHELL=$(echo $SHELL | awk -F  '/' '{print $NF}')
+
+if [[ -z $(grep "source ~/.container_setup.sh" "$(echo $HOME)/.$(echo $USR_SHELL)rc") ]]; then 
+	echo "" >> ~/.$(echo $USR_SHELL)rc # Newline
+	echo "# For distrobox container setup:" >> ~/.$(echo $USR_SHELL)rc
+	echo "source ~/.container_setup.sh" >> ~/.$(echo $USR_SHELL)rc;
+fi
